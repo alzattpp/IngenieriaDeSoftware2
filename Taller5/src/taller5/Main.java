1f65c4069a3fb1e795517aefc37ffbb019fa871f
sergioalzate <sergioa.alzateg@autonoma.edu.co>
2024-08-31 20:47:17 -0500

    Taller 5

--- Taller5/src/taller5/Main.java
+++ Taller5/src/taller5/Main.java
@@ -8,13 +8,13 @@ public class Main {
         
         Producto producto1 = new ProductoFisico("Chocolatina", 2000, 10, 0.5);
         Producto producto2 = new ProductoDigital("Libro", 30000, 3, 25);
-        Producto producto3 = new ProductoMixto("Combo de Regalo", 50000, 2, 1.0, 15);
+        Producto producto3 = new ProductoMixto("Juego de mesa", 50000, 2, 1.0, 15);
 
         System.out.println("El precio total de " + producto1.getNombre() + ": " + gestion.HacerCalculo(producto1));
         System.out.println("El precio total de " + producto2.getNombre() + ": " + gestion.HacerCalculo(producto2));
         System.out.println("El precio total de " + producto3.getNombre() + ": " + gestion.HacerCalculo(producto3));
 
-        ProductoFisico fisico1 = new ProductoFisico("Mochila", 100000, 1, 2.0);
+        ProductoFisico fisico1 = new ProductoFisico("Bolso", 100000, 1, 2.0);
         fisico1.enviarPorCorreo();
 
         ProductoMixto fisico2 = new ProductoMixto("Caja de Regalo", 70000, 3, 1.5, 30);
